1d5d7947e271e4ad0bae8a7f5b2eed1e756c5f94
Francy <webarbeit@gmail.com>
2011-11-29 13:58:11 +0100

    Added getCenterPoint() to shapes

--- shape.js
+++ shape.js
@@ -1,37 +1,54 @@
 /*
-	HTML5 Canvas - Shape Class V 1.1
+	HTML5 Canvas - Shape Class V 1.2
 	https://github.com/webarbeit
 */
 // Base Class
-Shape = function(_context, _x, _y, _color) {
-	this.init(_context, _x, _y, _color);
+Shape = function(_context, _x, _y, _color, _w, _h) {
+	this.init(_context, _x, _y, _color, _w, _h);
 }
-Shape.prototype.init = function(_context, _x, _y, _color) {
+Shape.prototype.init = function(_context, _x, _y, _color, _w, _h) {
 	this.x = _x || 0;
 	this.y = _y || 0;
+	this.width = _w || 0;
+	this.height = _h || 0;
 	this.color = _color;
 	this.canvasContext = _context;
+};
+//
+Shape.prototype.getCenterPoint = function() {
+	var centerX = this.x + (this.width / 2);
+	var centerY = this.y + (this.height / 2);
+	return { x: centerX, y: centerY };
 };
 
 // -------------------------------------------------
 Circle = function(_settings) {
 	this.constructor(_settings.context, _settings.x, _settings.y, _settings.color);
 	this.radius = _settings.radius;
+	this.alignToCenter = _settings.alignToCenter || false;
 };
 Circle.prototype = new Shape();
 //
 Circle.prototype.draw = function() {
+	this.canvasContext.save();
+	if(this.alignToCenter)
+		this.canvasContext.translate(this.radius / 2, this.radius / 2);
 	this.canvasContext.beginPath();
 	this.canvasContext.fillStyle = this.color;
 	this.canvasContext.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
 	this.canvasContext.fill();
+
+	this.canvasContext.restore();
+};
+Circle.prototype.getCenterPoint = function() {
+	var centerX = this.x + (this.radius / 2);
+	var centerY = this.y + (this.radius / 2);
+	return { x: centerX, y: centerY };
 };
 
 // -------------------------------------------------
 Rectangle = function(_settings) {
-	this.constructor(_settings.context, _settings.x, _settings.y, _settings.color);
-	this.width = _settings.width;
-	this.height = _settings.height;
+	this.constructor(_settings.context, _settings.x, _settings.y, _settings.color, _settings.width, _settings.height);
 };
 Rectangle.prototype = new Shape();
 //
@@ -42,9 +59,7 @@ Rectangle.prototype.draw = function() {
 
 // -------------------------------------------------
 ImageShape = function(_settings) {
-	this.constructor(_settings.context, _settings.x, _settings.y);
-	this.width = _settings.width;
-	this.height = _settings.height;
+	this.constructor(_settings.context, _settings.x, _settings.y, _settings.width, _settings.height);
 	this.src = _settings.src;
 	this.img = new Image();
 	this.img.src = this.src;
